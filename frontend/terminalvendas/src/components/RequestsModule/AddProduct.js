460f1b0d6b37c7af8b91d9ab3986cb0d739b7013
Leandro <leandrosf934@gmail.com>
2020-11-04 16:02:47 -0300

    atualização pedidos

--- frontend/terminalvendas/src/components/RequestsModule/AddProduct.js
+++ frontend/terminalvendas/src/components/RequestsModule/AddProduct.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
-
 import api from '../services/api';
 import { Link, Redirect } from 'react-router-dom';
+import { FaRegPlusSquare } from 'react-icons/fa';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -34,11 +34,6 @@ export default function AddProduct() {
       <div className="container">
         <div>
           <h2>Selecione o Produto </h2>
-          <Link to="/cadastrarProduto">
-            <button type="button" className="btn btn-primary">
-              Adicionar
-            </button>
-          </Link>
         </div>
         <table className="table table-hover table-dark">
           <thead>
@@ -68,43 +63,19 @@ export default function AddProduct() {
                   <td>{valor_custo}</td>
                   <td>{valor_venda}</td>
                   <td>
-                    {/* <Link to={`/novopedido`}> */}
                     <button
+                      onClick={handleAddProduct}
                       value={id}
                       type="button"
                       class="btn btn-outline-success"
                     >
-                      <svg
-                        value={id}
-                        onClick={handleAddProduct}
-                        style={{ color: 'green' }}
-                        width="3em"
-                        height="2em"
-                        viewBox="0 0 16 16"
-                        class="bi bi-plus-circle"
-                        fill="currentColor"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        {id}
-                        <path
-                          value={id}
-                          fill-rule="evenodd"
-                          d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"
-                        />
-
-                        <path
-                          value={id}
-                          fill-rule="evenodd"
-                          d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"
-                        />
-                      </svg>
+                      {id}
                     </button>
                     {redirectCheck === true ? (
                       <Redirect to="/novopedido" />
                     ) : (
                       redirectCheck
                     )}
-                    {/* </Link> */}
                   </td>
                 </tr>
               );
